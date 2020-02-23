e2e4b7d660430784240abcc1853d1069b4944cfa
erobertsdev <elias@eroberts.dev>
2020-02-23 08:04:56 -0700

    removed local fontawesome import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './fontawesome/css/fontawesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
